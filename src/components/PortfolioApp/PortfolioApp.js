d3f0db855002db77c13e21c460376072447fd6c3
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-06 22:49:35 -0500

    Adding Image component in

--- src/components/PortfolioApp/PortfolioApp.js
+++ src/components/PortfolioApp/PortfolioApp.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import styles from './PortfolioApp.css';
+import Image from '../Image';
 
 class PortfolioApp extends Component {
   render() {
     return (
       <div className={styles.PortfolioApp}>
+        <Image src="https:unsplash.it/700/400" />
         <h1 className={styles.header}>It Works!</h1>
         <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
         <p>A work in progess by Jesse!</p>

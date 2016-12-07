cc4ecf5bb9f450d70ad3fdf971dcaa7c716d797f
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-06 21:45:17 -0500

    to app

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './app.jsx';
+import PortfolioApp from '../components/PortfolioApp';
 
-render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
+render( <AppContainer><PortfolioApp/></AppContainer>, document.querySelector("#app"));
 
 if (module && module.hot) {
-  module.hot.accept('./app.jsx', () => {
-    const App = require('./app.jsx').default;
+  module.hot.accept('../components/PortfolioApp', () => {
+    const App = require('../components/PortfolioApp').default;
     render(
       <AppContainer>
         <App/>

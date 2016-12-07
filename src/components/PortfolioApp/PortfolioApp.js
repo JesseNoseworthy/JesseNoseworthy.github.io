24a19b9e485defa798e4075a01299f3bcc57475d
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-06 22:43:28 -0500

    className update for header

--- src/components/PortfolioApp/PortfolioApp.js
+++ src/components/PortfolioApp/PortfolioApp.js
@@ -5,7 +5,7 @@ class PortfolioApp extends Component {
   render() {
     return (
       <div className={styles.PortfolioApp}>
-        <h1 className={styles.tes}>It Works!</h1>
+        <h1 className={styles.header}>It Works!</h1>
         <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
         <p>A work in progess by Jesse!</p>
       </div>

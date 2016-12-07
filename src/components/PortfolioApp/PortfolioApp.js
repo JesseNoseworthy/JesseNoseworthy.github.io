defaf13e39adce1fa6fbb5e7cc01874f7b9288b3
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-06 22:00:48 -0500

    decouple

---
+++ src/components/PortfolioApp/PortfolioApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from '../../index.scss';
+
+class PortfolioApp extends Component {
+  render() {
+    return (
+      <div className="styles.app">
+        <h1>It Works!</h1>
+        <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
+        <p>A work in progess by Jesse!</p>
+      </div>
+    )
+  }
+}
+
+export default PortfolioApp;

0309c3df320a6ecd9ab1761b560507940f538459
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-06 22:42:42 -0500

    cleaner import

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import ReactDOM from 'react-dom';
 import PortfolioApp from '../components/PortfolioApp';
 
-render( <AppContainer><PortfolioApp/></AppContainer>, document.querySelector("#app"));
-
-if (module && module.hot) {
-  module.hot.accept('../components/PortfolioApp', () => {
-    const App = require('../components/PortfolioApp').default;
-    render(
-      <AppContainer>
-        <App/>
-      </AppContainer>,
-      document.querySelector("#app")
-    );
-  });
-}
+ReactDOM.render(
+  <PortfolioApp/>,
+  document.getElementById('app')
+);

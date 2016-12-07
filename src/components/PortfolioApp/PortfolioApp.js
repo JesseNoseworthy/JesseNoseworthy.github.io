a2bf7392cbdf359ad23a4a4c3476ebba078ee082
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-06 22:28:28 -0500

    local styles

--- src/components/PortfolioApp/PortfolioApp.js
+++ src/components/PortfolioApp/PortfolioApp.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import styles from '../../index.scss';
+import styles from './PortfolioApp.css';
 
 class PortfolioApp extends Component {
   render() {
     return (
-      <div className={styles.app}>
-        <h1>It Works!</h1>
+      <div className={styles.PortfolioApp}>
+        <h1 className={styles.tes}>It Works!</h1>
         <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
         <p>A work in progess by Jesse!</p>
       </div>

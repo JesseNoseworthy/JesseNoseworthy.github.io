c94f81d541c95e98c343c4a2dc4d3f11e4d7976f
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-11 17:09:58 -0500

    h1 has no className

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -10,7 +10,7 @@ class Hero extends Component {
       <div className={styles.heroContainer}>
         <Image variant="hero" src="https:unsplash.it/700/400" />
         <div className={styles.heroText}>
-          <h1 className={styles.header}>It Works!</h1>
+          <h1>It Works!</h1>
           <p>A work in progess by Jesse! Follow me on <Link href="https://github.com/JesseNoseworthy">GitHub</Link></p>
           </div>
       </div>

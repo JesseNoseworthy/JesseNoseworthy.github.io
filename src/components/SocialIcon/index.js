d21ec44a7b773baf3be1e6d5825491ad9978f69f
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-17 17:07:43 -0500

    social icon component

---
+++ src/components/SocialIcon/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import styles from './SocialIcon.scss';
+import classNames from 'classnames';
+import FontAwesome from 'react-fontawesome';
+import Link from '../Link';
+
+class SocialIcon extends Component {
+  render() {
+    const {
+      socialUrl,
+      variant,
+      iconType,
+      iconSize,
+      ...others
+    } = this.props
+
+    const iconClass = classNames(styles.socialIcon, styles[variant]);
+
+    return (
+      <Link href={socialUrl} {...others}>
+        <FontAwesome
+          className={iconClass}
+          name={iconType}
+          size={iconSize}
+         />
+      </Link>
+    )
+  }
+}
+
+SocialIcon.propTypes = {
+  socailUrl: React.PropTypes.string,
+  variant: React.PropTypes.string,
+  iconType: React.PropTypes.string.isRequired,
+  iconSize: React.PropTypes.any,
+}
+
+SocialIcon.getDefaultProps = {
+  iconType: 'facebook',
+  iconSize: '2x'
+}
+
+export default SocialIcon;

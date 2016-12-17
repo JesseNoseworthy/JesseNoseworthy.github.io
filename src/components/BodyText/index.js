185a3ce904bf777811bfb71af8202ce45c6e3e75
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-17 14:17:14 -0500

    Body Text component

---
+++ src/components/BodyText/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import styles from './BodyText.css';
+
+class BodyText extends Component {
+  render() {
+    const {
+      title, 
+      subTitle, 
+      content,
+      ...others
+    } = this.props
+
+    return (
+      <div className={styles.BodyText}>
+        { title ? <h1>{title}</h1> : null}
+        { subTitle ? <h2>{subTitle}</h2> : null}
+        { content ? <p>{content}</p> : null}
+      </div>
+    )
+  }
+}
+
+BodyText.propTypes = {
+  title: React.PropTypes.string,
+  subTitle: React.PropTypes.string,
+  content: React.PropTypes.string,
+};
+
+export default BodyText;

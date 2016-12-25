c8b34fc955085922b9e64c8e8322881b40adef9e
Jesse Noseworthy <noseworthyjesse@gmail.com>
2016-12-25 16:36:15 -0500

    import fixed

--- src/components/PortfolioApp/PortfolioApp.js
+++ src/components/PortfolioApp/PortfolioApp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styles from './PortfolioApp.scss';
-import globalStyles from '../../globalStyles/index.scss';
+import '../../globalStyles/index.scss';
 import SocialIconGroup from '../SocialIconGroup';
 import appData from '../../app/data/data.json';
 

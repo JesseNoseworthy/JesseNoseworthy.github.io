8de260400fc29d7555e35727232842e1d61cbf75
Jesse Noseworthy <noseworthyjesse@gmail.com>
2017-06-18 14:07:21 -0400

    Clean-up for the hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import styles from './Hero.scss';
-import NextButton from '../NextButton';
-import BodyText from '../BodyText';
 import Image from '../Image';
 
 const Hero = ({ responsiveFlags }) => (
   <div className={styles.heroContainer}>
     <div className={styles.content}>
       <Image className={styles.headshot} src={require('../../app/assets/headshot-rectangle.png')}/>
-      <h1><span className="highlight">Hello!</span> My name is Jesse Noseworthy and I'm a Web Developer</h1>
-      <NextButton 
-        iconDirection='down' 
-        destination='#'
-      />
+      <h1><span className="highlight">Hi,</span> I'm Jesse and I'm a Web Developer</h1>
     </div>
   </div>
 );

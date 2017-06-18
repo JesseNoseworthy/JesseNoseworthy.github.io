bd20a803329c7c053f15f2714a5a131859778875
Jesse Noseworthy <noseworthyjesse@gmail.com>
2017-06-18 14:34:30 -0400

    social icons

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import styles from './Hero.scss';
 import Image from '../Image';
 import About from '../About';
+import SocialIcon from '../SocialIcon';
 
 const Hero = ({ responsiveFlags }) => (
   <div className={styles.heroContainer}>
     <div className={styles.content}>
       <Image className={styles.headshot} src={require('../../app/assets/headshot-rectangle.png')}/>
       <h1><span className="highlight">Hi,</span> I'm Jesse and I'm a Web Developer</h1>
+      <div className={styles.SocialIconContainer}>
+        <SocialIcon iconType='twitter' socialUrl='#' />
+        <SocialIcon iconType='github' socialUrl='#' />
+        <SocialIcon iconType='linkedin' socialUrl='#' />
+        <SocialIcon iconType='envelope' socialUrl='#' />
+      </div>
     </div>
     <About responsiveFlags={responsiveFlags} />
   </div>

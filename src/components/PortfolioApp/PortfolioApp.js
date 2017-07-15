5a535a3912d2a3ca8a7903fb7b5993f2fbd40e9d
Jesse Noseworthy <noseworthyjesse@gmail.com>
2017-07-15 12:45:06 -0400

    Clean up

--- src/components/PortfolioApp/PortfolioApp.js
+++ src/components/PortfolioApp/PortfolioApp.js
@@ -1,42 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './PortfolioApp.scss';
 import '../../globalStyles/index.scss';
 import appData from '../../app/data/data.json';
-import {
-  isDesktop,
-  isTablet,
-  isMobile
-} from '../../utils';
-import Hero from '../Hero';
-import About from '../About';
 
-const PortfolioApp = React.createClass({
-  updateDimensions() {
-    this.setState({
-      isDesktop: isDesktop(),
-      isTablet: isTablet(),
-      isMobile: isMobile()
-    });
-  },
-  componentWillMount() {
-    this.updateDimensions();
-  },
-  componentDidMount() {
-    window.addEventListener('resize', this.updateDimensions);
-  },
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions);
-  },
+class PortfolioApp extends Component {
   render() {
-    const { isDesktop, isTablet, isMobile } = this.state;
-    const responsiveFlags = { isDesktop, isTablet, isMobile };
-
     return (
       <div className={styles.PortfolioApp}>
-        <Hero responsiveFlags={responsiveFlags} />
+        <h1>Hello world</h1>
       </div>
-    )
+    );
   }
-});
+}
 
 export default PortfolioApp;

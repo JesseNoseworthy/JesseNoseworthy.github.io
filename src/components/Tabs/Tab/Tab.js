3ac3b8ad96636fdde89042a1a47dc767f7c38d0e
Jesse Noseworthy <noseworthyjesse@gmail.com>
2018-04-02 22:22:36 -0400

    Tabs clean-up: conditional cursor, remove transition

--- src/components/Tabs/Tab/Tab.js
+++ src/components/Tabs/Tab/Tab.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import media from 'utils/media';
 
 const Container = styled.button`
   background: none;
   border: none;
   color: ${props => props.isActive ? props.theme.colors.primary : props.theme.colors.white};
-  transition: ${props => `color ${props.theme.values.baseTransition}`};
   font-size: 1.2rem;
-  cursor: pointer;
   margin: 0 5px;
   height: ${props => `calc(${props.theme.values.navBarHeight} - (${props.theme.values.navBarHeight} / 4))`};
   width: 100%;
   text-align: left;
+
+  ${props => !props.isActive && css`
+    cursor: pointer;
+  `};
 
   ${media.desktop`
     height: initial;

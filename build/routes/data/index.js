3e142aa155d9fae0d227c87848e976785444ce82
Jesse Noseworthy <jesse.noseworthy@shopify.com>
2020-07-08 14:42:45 -0400

    follow create-react-app

--- build/routes/data/index.js
+++
@@ -1,9 +0,0 @@
-import about from 'app/routes/data/about';
-import portfolio from 'app/routes/data/portfolio';
-import blog from 'app/routes/data/blog';
-
-module.exports = {
-  about,
-  portfolio,
-  blog
-};
